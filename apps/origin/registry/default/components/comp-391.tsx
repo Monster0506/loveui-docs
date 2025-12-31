249a98431cfc6bf02a9b9a43bd4631d053b32c8d
loveconnor <loveconnor2005@gmail.com>
2025-12-31 18:47:51 -0500

    update

--- apps/origin/registry/default/components/comp-391.tsx
+++
@@ -1,9 +0,0 @@
-import { Avatar, AvatarFallback } from "@/registry/default/ui/avatar"
-
-export default function Component() {
-  return (
-    <Avatar>
-      <AvatarFallback>CL</AvatarFallback>
-    </Avatar>
-  )
-}
